7218a0431781eacaa9c912b5b1fbb77aab2b19f6
jernej.jerebic <jernej.jerebic@616293e0-0337-0410-8baa-49d44c8c4e6c>
2007-08-09 10:07:33 +0000

    server is complete

---
+++ src/ConnectionProcessor.java
@@ -0,0 +1,120 @@
+import java.io.*;
+import javax.net.ssl.*;
+import java.util.StringTokenizer;
+
+public class ConnectionProcessor implements Runnable
+{
+    private Server server;
+    private SSLSocket sslsocket;
+    private DataInputStream din;
+    private DataOutputStream dout;
+    private String name;
+    
+    public ConnectionProcessor( Server server, SSLSocket sslsocket )
+    {
+	this.server = server;
+	this.sslsocket = sslsocket;
+	
+	try
+	{
+	    this.name = sslsocket.getSession().getPeerPrincipal().getName();
+	}
+	catch (SSLPeerUnverifiedException ex)
+	{
+	    ex.printStackTrace();
+	    System.exit( 1 );
+	}
+
+	new Thread( this ).start();
+    }
+
+    private synchronized String processPosting( String s )
+    {
+	int delta;
+	String izdelek;
+	
+	/*
+	 * dodaj izdelek x kolicine y
+	 */
+	
+	if(s.charAt(0) == 'D' && s.charAt(1) == ' ')
+	{
+	    Skladisce tmp = server.najdiSkladisce(name);
+	    StringTokenizer st = new StringTokenizer(s);
+	    
+	    if(st.countTokens() != 3 || tmp == null)
+	    	return null;
+	    
+	    st.nextToken();
+	    izdelek = st.nextToken();
+	    delta = Integer.parseInt(st.nextToken());
+	    tmp.dodaj(izdelek, delta );
+	    
+	    return "Dodaj " + izdelek + " za " + delta + " enot. Stanje skladisca je:"+tmp.vrniStanje();
+	}
+	else if(s.charAt(0) == 'Z' && s.charAt(1) == ' ')
+	{
+	    Skladisce tmp = server.najdiSkladisce(name);
+	    StringTokenizer st = new StringTokenizer(s);
+	    
+	    if(st.countTokens() != 3 || tmp == null)
+	    	return null;
+	    
+	    st.nextToken();
+	    izdelek = st.nextToken();
+	    delta = Integer.parseInt(st.nextToken());
+	   
+	    if( tmp.vrniStanje(izdelek)-delta < 0 )
+	    	return "Ni dovolj zaloge za izdelek "+ izdelek +" . Stanje skladisca je:"+tmp.vrniStanje();
+	    else
+	    	tmp.zmanjsaj(izdelek, delta);
+	    return "Zmanjsaj " + izdelek +" za " + delta + " enot. Stanje skladišèa je:"+tmp.vrniStanje();
+	   
+	}
+	else if(s.charAt(0) == 'K' && s.charAt(1) == ' ')
+	    return "Prekinjam...";
+    
+	return null;
+    }
+
+    public void run()
+    {
+	String response;
+	Skladisce tmp;
+	
+	try
+	{
+	    InputStream in = sslsocket.getInputStream();
+	    OutputStream out = sslsocket.getOutputStream();
+
+	    din = new DataInputStream( in );
+	    dout = new DataOutputStream( out );
+	    
+	    if((tmp = server.najdiSkladisce(name)) == null)
+		tmp = server.dodajSkladisce(name);
+
+	    dout.writeUTF("Pozdravljen(a) " + name + ". Stanje skladisca je:" + tmp.vrniStanje());
+	    
+	    while( true )
+	    {
+		if((response = processPosting(din.readUTF())) != null)
+		    dout.writeUTF( response );
+		else
+		    dout.writeUTF( "Napaka! Prekinjam..." );
+	    }
+	}
+	catch( IOException ie )
+	{
+	    try
+	    {
+		sslsocket.close();
+	    }
+	    catch( IOException ie2 )
+	    {
+		System.out.println( "Error closing socket "+sslsocket );
+	    }
+
+	    System.out.println( "Closed connection from socket "+sslsocket );
+	}
+    }
+}
---
+++ src/Server.java
@@ -0,0 +1,136 @@
+import java.io.*;
+import java.security.*;
+import javax.net.ssl.*;
+
+public class Server implements Runnable
+{
+	private int port;
+	private KeyStore clientKeyStore;
+	private KeyStore serverKeyStore;
+	private SSLContext sslContext;
+	private char[] passphrase, publicPass;
+	private String otherJksFile, myJksFile;
+	static private SecureRandom secureRandom;
+	private String[] cs = {"TLS_RSA_WITH_AES_128_CBC_SHA"};
+	private Skladisce skladisca = null, last = null;
+	
+	public Server( int port, String myJksFile, String myPwd,
+			String otherJksFile, String otherPwd )
+	{
+		this.port = port;
+		this.myJksFile = myJksFile;
+		this.passphrase = myPwd.toCharArray();
+		this.otherJksFile = otherJksFile;
+		this.publicPass = otherPwd.toCharArray();	
+		
+		new Thread( this ).start();
+	}
+	
+	private void setupClientKeyStore()
+	throws GeneralSecurityException, IOException
+	{
+		clientKeyStore = KeyStore.getInstance( "JKS" );
+		clientKeyStore.load( new FileInputStream( otherJksFile ), publicPass );
+	}
+	
+	private void setupServerKeystore()
+	throws GeneralSecurityException, IOException
+	{
+		serverKeyStore = KeyStore.getInstance( "JKS" );
+		serverKeyStore.load( new FileInputStream( myJksFile ), passphrase );
+	}
+	
+	private void setupSSLContext()
+	throws GeneralSecurityException, IOException
+	{
+		TrustManagerFactory tmf = TrustManagerFactory.getInstance( "SunX509" );
+		tmf.init( clientKeyStore );
+		
+		KeyManagerFactory kmf = KeyManagerFactory.getInstance( "SunX509" );
+		kmf.init( serverKeyStore, passphrase );
+		
+		sslContext = SSLContext.getInstance( "TLS" );
+		sslContext.init( kmf.getKeyManagers(),
+				tmf.getTrustManagers(),
+				secureRandom );
+	}
+	
+	public void run()
+	{
+		try
+		{
+			setupClientKeyStore();
+			setupServerKeystore();
+			setupSSLContext();
+			
+			SSLServerSocketFactory sf = sslContext.getServerSocketFactory();
+			SSLServerSocket ss = (SSLServerSocket)sf.createServerSocket( port );
+			
+			// Specify allowed ciphers
+			ss.setEnabledCipherSuites(cs);
+			
+			// Require client authorization
+			ss.setNeedClientAuth( true );
+			
+			System.out.println( "Listening on port "+port+"..." );
+			
+			while (true)
+			{
+				SSLSocket sslsocket = (SSLSocket) ss.accept();
+				System.out.println( "Got connection from "+sslsocket );
+				
+				ConnectionProcessor cp = new ConnectionProcessor( this, sslsocket );
+			}
+		}
+		catch( GeneralSecurityException gse )
+		{
+			gse.printStackTrace();
+		}
+		catch( IOException ie )
+		{
+			ie.printStackTrace();
+		}
+	}
+	
+	public synchronized Skladisce dodajSkladisce(String name)
+	{
+		if(skladisca != null)
+		{
+			last.next = new Skladisce(name);
+			last = last.next;
+		}
+		else
+		{
+			skladisca = new Skladisce(name);
+			last = skladisca;
+		}
+		
+		return last;
+	}
+	
+	public synchronized Skladisce najdiSkladisce(String name)
+	{
+		Skladisce tmp = skladisca;
+		
+		while(tmp != null && !tmp.matches(name))
+			tmp = tmp.next;
+		
+		return tmp;
+	}
+	
+	static public void main( String args[] )
+	{
+		if (args.length != 5)
+		{
+			System.err.println( "Usage: java Server [port number] serverJksFile serverPwd otherJksFile otherPwd" );
+			System.exit( 1 );
+		}
+		
+		System.out.println( "Wait while secure random numbers are initialized...." );
+		secureRandom = new SecureRandom();
+		secureRandom.nextInt();
+		System.out.println( "Done." );
+		
+		new Server( Integer.parseInt(args[0]), args[1], args[2], args[3], args[4] );
+	}
+}
